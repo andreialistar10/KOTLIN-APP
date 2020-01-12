7506c8a87ea947edf455bf2d64533e732beaebf3
andreialistar10 <andrei.alistar98@gmail.com>
2020-01-12 15:33:39 +0200

    Implement show current location and set camera view on my current location when map shows

--- app/src/main/java/com/andrei/entities/components/map/MapFragment.kt
+++ app/src/main/java/com/andrei/entities/components/map/MapFragment.kt
@@ -1,23 +1,31 @@
 package com.andrei.entities.components.map
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.andrei.entities.R
 import com.andrei.entities.core.TAG
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.map_fragment.*
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,9 +53,31 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
         googleMap.let {
             this.googleMap = it
-            val dhaka = LatLng(23.777176, 90.399452)
-            it.addMarker(MarkerOptions().position(dhaka).title("Mergee baaa"))
-            it.moveCamera(CameraUpdateFactory.newLatLng(dhaka))
+            it.uiSettings.isMyLocationButtonEnabled = true
+            it.setMinZoomPreference(15.0f)
+            val ctx = context
+            if (ctx != null && ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                setCameraOnCurrentLocation(it)
+            }
+        }
+    }
+
+    private fun setCameraOnCurrentLocation(it: GoogleMap) {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(parentFragment!!.activity as Activity)
+        it.isMyLocationEnabled = true
+        val loc = fusedLocationClient.lastLocation
+        loc.addOnCompleteListener { location ->
+            val currentLocation = location.result
+            if (currentLocation != null)
+                it.moveCamera(
+                    CameraUpdateFactory.newLatLng(
+                        LatLng(
+                            currentLocation.latitude,
+                            currentLocation.longitude
+                        )
+                    )
+                )
         }
     }
 }
